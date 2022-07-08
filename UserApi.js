17d89cb25a4fd247b86fb5ed5ddbb73e3902fb89
Porsa <porsa@mail.ru>
2022-07-08 15:33:32 +0300

    changing name on server is real

--- UserApi.js
+++ UserApi.js
@@ -1,9 +1,28 @@
 class UserApi {
   constructor() {
     this.usersUrl = `${BASE_URL}/${endpoints.users}`;
+    this.id = USER_ID;
   }
-  async loadUser(id) {
-    let response = await fetch(`${this.usersUrl}/${id}`);
+  async loadUser() {
+    let response = await fetch(`${this.usersUrl}/${this.id}`);
+    let user = await response.json();
+    return user;
+  }
+  async loadNewUser(name, about) {
+    let response = await fetch(
+      `http://localhost:5000/users/62a11004753707ba8cdc5b8f`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({
+          name: `${name}`,
+          about: `${about}`,
+        }),
+      }
+    );
+    console.log(response);
     let user = await response.json();
     return user;
   }
--- script.js
+++ script.js
@@ -1,68 +1,55 @@
-const BASE_URL = `http://127.0.0.1:5000`;
-const endpoints = {
-  users: 'users',
-  cards: 'cards',
-};
-const place = document.querySelector(`.places-list`);
-const greyArea = document.querySelector(`.grey_area`);
-const editBtn = document.querySelector(`.user-info__edit`);
-const editWindow = document.querySelector(`.edit`);
-const cardWindow = document.querySelector(`.popup`);
-const mainPopupBtn = document.querySelector(`.user-info__button`);
-const editInputForm = document.querySelector(`.edit__form`);
-const popupInputForm = document.querySelector(`.popup__form`);
-const editSubmitBtn = document.querySelector(`.edit__button`);
-const cardSubmitBtn = document.querySelector(`.popup__button`);
-const cardUrl = document.querySelector(`.popup__input_type_link-url`);
-const cardName = document.querySelector(`.popup__input_type_name`);
-const cardArray = [];
+(async function () {
+  const test = new UserApi();
+  const user = await test.loadUser();
+  console.log(user);
 
-const test = new UserApi();
-const user = test.loadUser(`62a11004753707ba8cdc5b8f`);
-const newUser = new UserNewApi(`62a11004753707ba8cdc5b8f`);
-lastVersionOfUser = newUser.loadNewUser();
-const cardtest = new AllCardsApi();
-const cardsall = cardtest.loadCards();
-const loadName = new UserInfo(editWindow);
-const editForm = new Popup(editWindow, editBtn);
-const popupForm = new Popup(cardWindow, mainPopupBtn);
-const closeBigImg = new ImgPopup();
-const createErrors = new Errors(editInputForm, popupInputForm);
-const validationForm = new FormValidator(
-  editInputForm,
-  editSubmitBtn,
-  popupInputForm,
-  cardSubmitBtn
-);
-
-closeBigImg.setEventListeners();
-validationForm.setEventListeners();
-createErrors.setEventListeners();
-
-const everythingAboutForms = () => {
-  const openAndCloseForms = () => {
-    editForm.open();
-    popupForm.open();
+  const updateUserData = (name, about) => {
+    test.loadNewUser(name, about);
   };
-  openAndCloseForms();
 
-  const resultForms = () => {
-    editSubmitBtn.addEventListener(`click`, (event) => {
-      event.preventDefault();
-      loadName.updateUserInfo();
-      editForm.enter();
-    });
+  const cardtest = new AllCardsApi();
+  const cardsall = cardtest.loadCards();
+  const loadName = new UserInfo(editWindow, updateUserData);
+  const editForm = new Popup(editWindow, editBtn);
+  const popupForm = new Popup(cardWindow, mainPopupBtn);
+  const closeBigImg = new ImgPopup();
+  const createErrors = new Errors(editInputForm, popupInputForm);
+  const validationForm = new FormValidator(
+    editInputForm,
+    editSubmitBtn,
+    popupInputForm,
+    cardSubmitBtn
+  );
 
-    cardSubmitBtn.addEventListener(`click`, (event) => {
-      event.preventDefault();
-      const userCardInstance = new Card(cardName.value, cardUrl.value);
-      const userCardNode = userCardInstance.render();
-      place.prepend(userCardNode);
-      popupForm.enter();
-    });
+  closeBigImg.setEventListeners();
+  validationForm.setEventListeners();
+  createErrors.setEventListeners();
+
+  const everythingAboutForms = () => {
+    const openAndCloseForms = () => {
+      editForm.open();
+      popupForm.open();
+    };
+    openAndCloseForms();
+
+    const resultForms = () => {
+      editSubmitBtn.addEventListener(`click`, (event) => {
+        event.preventDefault();
+        loadName.updateUserInfo();
+        editForm.enter();
+      });
+
+      cardSubmitBtn.addEventListener(`click`, (event) => {
+        event.preventDefault();
+        const userCardInstance = new Card(cardName.value, cardUrl.value);
+        const userCardNode = userCardInstance.render();
+        place.prepend(userCardNode);
+        popupForm.enter();
+      });
+    };
+    resultForms();
   };
-  resultForms();
-};
-everythingAboutForms();
+  everythingAboutForms();
 
-//чтобы использовать PATCH нам нужно знать id пользователя, его нужно достать из UserApi, а дальше использовать в PATCH
+  //чтобы использовать PATCH нам нужно знать id пользователя, его нужно достать из UserApi, а дальше использовать в PATCH
+})();
